14a0806a949c7fa5984ddc93d8b379f9270378c8
rasakereh <rasakereh@manmail.ir>
2019-06-29 19:15:56 +0430

    Subpopulations considered

--- code/fetch.R
+++ code/fetch.R
@@ -1,6 +1,7 @@
 require(GEOquery)
 require(dplyr)
 require(ggplot2)
+require(pheatmap)
 
 setwd("~/Desktop/sharif/9702/Bio/project")
 
@@ -10,19 +11,30 @@ PLATFORM <- "GPL6244"
 gset <- getGEO(ACCESSION_NO, GSEMatrix =TRUE, AnnotGPL=TRUE, destdir="data/")
 if (length(gset) > 1) idx <- grep(PLATFORM, attr(gset, "names")) else idx <- 1
 gset <- gset[[idx]]
-gsms <- paste0("00000000000000000000000000000000000000001000100000",
-               "00000000000000000010100010111101001100110101010101",
-               "00010001000000000000000000000000000001111111001000",
-               "11111111111111111111")
-groups <- strsplit(gsub("1", "Healthy-", gsub("0", "AML-", gsms)), '-')[[1]]
+gsms <- paste0("00000000000002222232322322332222233232364236423626",
+               "62662662262262262272162242455721621162116721672168",
+               "33683368336322222222232222222333333334444444001000",
+               "77777771555541111111")
+categories <- c("AML Primary", "Normal T", "AML B", "AML T", "Normal G", "Normal Mono",
+            "AML Cell", "Normal B", "Normal CD34")
+for(i in 1:length(categories))
+{
+  gsms <- gsub(as.character(i-1), paste0(categories[i], "-"), gsms)
+  print(gsms)
+}
+groups <- strsplit(gsms, '-')[[1]]
 
 expressionData <- exprs(gset) %>% na.omit()
-resPCA <- prcomp(expressionData)
-
+centeredExpression <- expressionData %>% t() %>% scale(scale=F) %>% t()
+resPCA <- prcomp(centeredExpression)
 # (D)ata * (R)otation = (X)
-# D: 4577*170, rows: d_i, cols: g_i
+# D: 4577*170, rows: g_i, cols: d_i
 # R: 170*170, rows: g_i, rows: pc_i
-# X: 4577*170, rows: d_i, rows: pc_i
+# X: 4577*170, rows: pc_i, rows: d_i
 
-plot(resPCA$x[,1], resPCA$x[,2])
+groupedPCA <- data.frame(resPCA$rotation, Category=groups)
 
+ggplot(groupedPCA, aes(PC1, PC2, color=Category)) + geom_point(size=2.8) + theme_bw()
+pdf("results/heatmap", height=30, width=30)
+pheatmap(cov(resPCA$rotation %>% t()))
+dev.off()
