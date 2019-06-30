fcec961ce4a358d8c42efda9e149499536ea8e68
rasakereh <rasakereh@manmail.ir>
2019-06-30 15:53:27 +0430

    I'm overwhelmed by the groups similarity

--- code/fetch.R
+++ code/fetch.R
@@ -21,12 +21,13 @@ centeredExpression <- expressionData %>% t() %>% scale(scale=F) %>% t()
 resPCA <- prcomp(centeredExpression)
 # (D)ata * (R)otation = (X)
 # D: 4577*170, rows: g_i, cols: d_i
-# R: 170*170, rows: g_i, rows: pc_i
-# X: 4577*170, rows: pc_i, rows: d_i
+# R: 170*170, rows: d_i, cols: pc_i
+# X: 4577*170, rows: g_i, cols: pc_i
 
 groupedPCA <- data.frame(resPCA$rotation, Category=groups)
-
+boxplot(expressionData)
 ggplot(groupedPCA, aes(PC1, PC2, color=Category)) + geom_point(size=2.8) + theme_bw()
-pdf("results/heatmap", height=30, width=30)
-pheatmap(cov(resPCA$rotation %>% t()))
+pdf("results/corHeatmap.pdf", width=20, height=20)
+pheatmap(cor(expressionData))
 dev.off()
+
