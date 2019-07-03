2ae387f9a35894ce237f2abe32fac1cd9506bf25
rasakereh <rasakereh@manmail.ir>
2019-07-03 07:31:20 +0430

    Significant Gene extraction

---
+++ code/analyze.R
@@ -0,0 +1,73 @@
+require(GEOquery)
+require(dplyr)
+require(udpipe)
+require(limma)
+
+setwd("~/Desktop/sharif/9702/Bio/project")
+
+ACCESSION_NO <- "GSE48558"
+PLATFORM <- "GPL6244"
+
+gset <- getGEO(ACCESSION_NO, GSEMatrix =TRUE, AnnotGPL=TRUE, destdir="data/")
+if (length(gset) > 1) idx <- grep(PLATFORM, attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+GENE.SYMBOLS <- data.frame(Gene.symbol=fData(gset)["Gene symbol"])
+GENE.SYMBOLS <- GENE.SYMBOLS %>% mutate(probe_id=rownames(GENE.SYMBOLS))
+GENE.TITLES <- data.frame(Gene.title=fData(gset)["Gene title"])
+GENE.TITLES <- GENE.TITLES %>% mutate(probe_id=rownames(GENE.TITLES))
+
+categories <- as.factor(gset$`phenotype:ch1`)
+subpopulations <- as.factor(gset$source_name_ch1)
+
+expressionData <- data.frame(exprs(gset) %>% na.omit())
+T.expr <- expressionData %>%
+  select(expressionData[which(grepl("T",subpopulations))] %>% colnames())
+T.cats <- categories[which(grepl("T",subpopulations))]
+B.expr <- expressionData %>%
+  select(expressionData[which(grepl("B",subpopulations))] %>% colnames())
+B.cats <- categories[which(grepl("B",subpopulations))]
+
+tTGenerator <- function(fl, exprData)
+{
+  design <- model.matrix(~ fl + 0, exprData)
+  colnames(design) <- levels(fl)
+  fit <- lmFit(exprData, design)
+  cont.matrix <- makeContrasts(Normal-Leukemia, levels=design)
+  fit2 <- contrasts.fit(fit, cont.matrix)
+  fit2 <- eBayes(fit2, 0.01)
+  tT <- topTable(fit2, adjust="fdr", sort.by="B", number=250)
+  if(!sum(grepl("Gene.symbol", colnames(tT))))
+  {
+    tT <- tT %>% mutate(probe_id=rownames(tT)) %>% left_join(GENE.SYMBOLS, by="probe_id") %>%
+      left_join(GENE.TITLES, by="probe_id")
+    rownames(tT) <- tT$probe_id
+  }
+  tT <- subset(tT, select=c("logFC", "adj.P.Val","P.Value","Gene.symbol","Gene.title"))
+  
+  return(tT)
+}
+
+tT.total <- tTGenerator(categories, expressionData)
+tT.B <- tTGenerator(B.cats, B.expr)
+tT.T <- tTGenerator(T.cats, T.expr)
+
+total.ups <- (tT.total %>% filter(adj.P.Val < 1e-4 & logFC > 1))[['Gene.symbol']] %>%
+  strsplit("//") %>% unlist() %>% unique()
+total.downs <- (tT.total %>% filter(adj.P.Val < 1e-4 & logFC < 1))[['Gene.symbol']] %>%
+  strsplit("//") %>% unlist() %>% unique()
+B.ups <- (tT.B %>% filter(adj.P.Val < 1e-4 & logFC > 1))[['Gene.symbol']] %>%
+  strsplit("//") %>% unlist() %>% unique()
+B.downs <- (tT.B %>% filter(adj.P.Val < 1e-4 & logFC < 1))[['Gene.symbol']] %>%
+  strsplit("//") %>% unlist() %>% unique()
+T.ups <- (tT.T %>% filter(adj.P.Val < 1e-4 & logFC > 1))[['Gene.symbol']] %>%
+  strsplit("//") %>% unlist() %>% unique()
+T.downs <- (tT.T %>% filter(adj.P.Val < 1e-4 & logFC < 1))[['Gene.symbol']] %>%
+  strsplit("//") %>% unlist() %>% unique()
+
+write(total.ups, file="results/TotalUps.txt")
+write(total.downs, file="results/TotalDowns.txt")
+write(B.ups, file="results/BUps.txt")
+write(B.downs, file="results/BDowns.txt")
+write(T.ups, file="results/TUps.txt")
+write(T.downs, file="results/TDowns.txt")
---
+++ code/visualization.R
@@ -0,0 +1,53 @@
+require(GEOquery)
+require(dplyr)
+require(ggplot2)
+require(pheatmap)
+require(plotly)
+require(Rtsne)
+
+setwd("~/Desktop/sharif/9702/Bio/project")
+
+ACCESSION_NO <- "GSE48558"
+PLATFORM <- "GPL6244"
+
+gset <- getGEO(ACCESSION_NO, GSEMatrix =TRUE, AnnotGPL=TRUE, destdir="data/")
+if (length(gset) > 1) idx <- grep(PLATFORM, attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+category <- as.factor(gset$`phenotype:ch1`)
+subpopulation <- as.factor(gset$source_name_ch1)
+
+expressionData <- exprs(gset) %>% na.omit()
+centeredExpression <- expressionData %>% t() %>% scale(scale=F) %>% t()
+resPCA <- prcomp(centeredExpression)
+resTSNE <- Rtsne(centeredExpression %>% t(), max_iter=300)
+# (D)ata * (R)otation = (X)
+# D: 4577*170, rows: g_i, cols: d_i
+# R: 170*170, rows: d_i, cols: pc_i
+# X: 4577*170, rows: g_i, cols: pc_i
+
+groupedPCA <- data.frame(resPCA$rotation, Category=category, Subpop=subpopulation)
+groupedTSNE <- data.frame(resTSNE$Y, Category=category, Subpop=subpopulation)
+
+PCA.T.compare <- groupedPCA %>% filter(grepl("T",Subpop))
+PCA.B.compare <- groupedPCA %>% filter(grepl("B",Subpop))
+SNE.T.compare <- groupedTSNE %>% filter(grepl("T",Subpop))
+SNE.B.compare <- groupedTSNE %>% filter(grepl("B",Subpop))
+
+pdf("results/BasicVisualisation.pdf")
+ggplot(PCA.T.compare, aes(PC1, PC2, color=Category)) + geom_point(size=2.8) +
+  labs(title="T Cells(PCA)") + theme_bw()
+ggplot(PCA.B.compare, aes(PC1, PC2, color=Category)) + geom_point(size=2.8) +
+  labs(title="B Cells(PCA)") + theme_bw()
+ggplot(groupedPCA, aes(PC1, PC2, color=Category)) + geom_point(size=2.8) +
+  labs(title="Mixture of Cells(PCA)") + theme_bw()
+ggplot(SNE.T.compare, aes(X1, X2, color=Category)) + geom_point(size=2.8) +
+  labs(title="T Cells(t-SNE)") + theme_bw()
+ggplot(SNE.B.compare, aes(X1, X2, color=Category)) + geom_point(size=2.8) +
+  labs(title="B Cells(t-SNE)") + theme_bw()
+ggplot(groupedTSNE, aes(X1, X2, color=Category)) + geom_point(size=2.8) +
+  labs(title="Mixture of Cells(t-SNE)") + theme_bw()
+dev.off()
+pdf("results/heatmap.pdf", height=30, width=30)
+pheatmap(cor(expressionData), labels_row=category, labels_col=category)
+dev.off()
